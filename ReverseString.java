ace0a3244fb7d51118627c2403beb5e7203b09d9
Jeeta-sri <srijeetadutta16@gmail.com>
2025-09-28 19:45:03 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,10 @@
+
+    public class ReverseString {
+	public static void main(String[] args) {
+		String str = "Innovation";
+		String rev = new StringBuilder(str).reverse().toString();
+		System.out.println("Reversed: " + rev);
+	}
+}
+
+
