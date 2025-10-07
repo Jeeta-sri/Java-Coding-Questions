6559adcda07bfbcae8c867673e31ca87f9e84b89
Jeeta-sri <srijeetadutta16@gmail.com>
2025-10-07 23:57:38 +0530

    Add files via upload

---
+++ GCD/gcd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class gcd {
+  static int gcd(int a, int b) {
+    if (b == 0)
+      return a;
+    return gcd(b, a % b);
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt(), b = sc.nextInt();
+    System.out.println("GCD = " + gcd(a, b));
+  }
+}
+
