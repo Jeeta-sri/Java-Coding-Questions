e639cc2b6345d3ff4f4d30d838efba8e8baa3a0f
Jeeta-sri <srijeetadutta16@gmail.com>
2025-10-10 14:38:57 +0530

    Add files via upload

---
+++ SubstringCheck.java
@@ -0,0 +1,16 @@
+
+    public class SubstringCheck {
+    public static void main(String[] args) {
+        String str = "Hello world";
+        String sub = "world";
+
+        if (str.contains(sub)) {
+            System.out.println("Substring is present!");
+        } else {
+            System.out.println("Substring is not present!");
+        }
+    }
+}
+
+
+
