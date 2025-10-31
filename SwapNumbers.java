c5e94e3621a83fad55325d306fe136d535044ac2
Jeeta-sri <srijeetadutta16@gmail.com>
2025-10-31 23:41:20 +0530

    Add files via upload

---
+++ SwapNumbers.java
@@ -0,0 +1,15 @@
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+
+        // Swapping using a temporary variable
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}
